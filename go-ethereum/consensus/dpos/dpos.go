b1a7dde61b4a1e8150f69fb8252d625c6e8ef503
dpndev <dev@dip.network>
2018-11-16 11:17:21 +0800

    fix Api getDetai

--- go-ethereum/consensus/dpos/dpos.go
+++ go-ethereum/consensus/dpos/dpos.go
@@ -33,7 +33,7 @@ const (
 
 	blockInterval    = int64(10)
 	epochInterval    = int64(86400)
-	maxValidatorSize = 21
+	maxValidatorSize = 3
 	safeSize         = maxValidatorSize*2/3 + 1
 	consensusSize    = maxValidatorSize*2/3 + 1
 )
